917cf1a9a05e2c13e3ddea28f6b08809c4cd357e
ASUS <ASUS@GOPYLAPTOP>
2019-03-01 16:38:21 +0530

    gopy

--- gncvb/newadmission.vb
+++ gncvb/newadmission.vb
@@ -2,4 +2,53 @@
 	Private Sub Label2_Click(sender As Object, e As EventArgs)
 
 	End Sub
+
+	Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs) Handles TextBox5.TextChanged
+
+	End Sub
+
+	Private Sub newadmission_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+	End Sub
+
+	Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+	End Sub
+
+	Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
+
+	End Sub
+
+	Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+	End Sub
+
+	Private Sub RichTextBox2_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox2.TextChanged
+
+	End Sub
+
+	Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+
+	End Sub
+
+	Private Sub Label28_Click(sender As Object, e As EventArgs) Handles Label28.Click
+
+	End Sub
+
+	Private Sub Label30_Click(sender As Object, e As EventArgs) Handles Label30.Click
+
+	End Sub
+
+	Private Sub TextBox13_TextChanged(sender As Object, e As EventArgs) Handles TextBox13.TextChanged
+
+	End Sub
+
+	Private Sub Label29_Click(sender As Object, e As EventArgs) Handles Label29.Click
+
+	End Sub
+
+	Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+		Me.Hide()
+
+	End Sub
 End Class
